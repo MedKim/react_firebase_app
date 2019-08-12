365c460db4c0a40ad104ca03abe5dce836c98487
github MedKim <stock.realestate.fx@gmail.com>
2019-08-12 14:32:26 +0900

    必要なファイル/ディレクトリ作成

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+
+class App extends React.Component {
+	render(){
+		return(
+	  <BrowserRouter>
+	  	<div className="App">
+			<Navbar />
+	  	</div>
+	  </BrowserRouter>
+  );
+	}
+}
+
+export default App;
