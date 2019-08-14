e372e551145209efa41dd4a97944c66ab9541a0b
github MedKim <stock.realestate.fx@gmail.com>
2019-08-14 09:09:45 +0900

    reducerの設定とconnect

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -13,3 +13,17 @@ const ProjectSummary = ()=>{
 }
 
 export default ProjectSummary
+import React from 'react';
+
+const ProjectSummary = ({project}) =>{
+	return(
+		<div className="card z-depth-0 project-summary">
+			<div className="card-content grey-text text-darken-3">
+				<span className="card-title">{project.title}</span>
+				<p className="grey-text">3rd August</p>
+			</div>
+		</div>
+	)
+}
+
+export default ProjectSummary
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,11 @@
-const initState = {}
+const initState = {
+   projects = [
+      {id: '1', title: 'help me find patch', content:'lkjj'},
+      {id: '2', title: 'egg me find patch', content:'lkjj'},
+      {id: '3', title: 'collet me find patch', content:'lkjj'},
+
+   ]
+}
 const projectReducer = (state = initState, action) =>{
    return state;
 }
